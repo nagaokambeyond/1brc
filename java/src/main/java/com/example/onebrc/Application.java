fcec8416dcb4e5bc530015f1cf0781d56e61651d
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2025-05-08 00:08:53 +0900

    public static void main(String[] args)

--- java/src/main/java/com/example/onebrc/Application.java
+++ java/src/main/java/com/example/onebrc/Application.java
@@ -1,29 +1,34 @@
+package com.example.onebrc;
+
 import com.example.onebrc.calculate.CalculateAverage;
+import com.example.onebrc.calculate.CalculateAverage2;
 
 import java.io.IOException;
 import java.util.Objects;
 
-void main() throws IOException {
-    final var url = CalculateAverage.class.getClassLoader().getResource("measurements.txt");
-    if (Objects.isNull(url)) {
-        System.out.println("ファイルが存在しません。");
-        return;
+class Application {
+    public static void main(String[] args) throws IOException {
+        final var url = Application.class.getClassLoader().getResource("measurements.txt");
+        if (Objects.isNull(url)) {
+            System.out.println("ファイルが存在しません。");
+            return;
+        }
+        final var path = url.getPath();
+        System.out.printf("ファイルの場所：%s%n", path);
+
+        long start = System.currentTimeMillis();
+
+        final var cls = new CalculateAverage2();
+        final var result = cls.getAverage(path);
+        result.keySet().stream().sorted().forEach(key -> {
+            // keyでソートして表示する
+            final var value = result.get(key);
+            System.out.printf("%s=%s¥n", key, value);
+        });
+
+        long end = System.currentTimeMillis();
+        System.out.println();
+        final var diff = end - start;
+        System.out.println(diff + "ms");
     }
-    final var path = url.getPath();
-    System.out.printf("ファイルの場所：%s%n", path);
-
-    long start = System.currentTimeMillis();
-
-    final var cls = new CalculateAverage();
-    final var result = cls.getAverage(path);
-    result.keySet().stream().sorted().forEach(key -> {
-        // keyでソートして表示する
-        final var value = result.get(key);
-        System.out.printf("%s=%s¥n", key, value);
-    });
-
-    long end = System.currentTimeMillis();
-    System.out.println();
-    final var diff = end - start;
-    System.out.println(diff + "ms");
 }
